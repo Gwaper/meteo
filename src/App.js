852f1872a9bf33a049197043047198449f1dd543
Julien Duhem <dhmjulien@gmail.com>
2019-03-25 12:30:43 +0100

    Fetching data, changing state with response

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const APIKey = 'RoHmJrMrbbo7p1SvsLfA2reOGUOQfi5F';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 0,
+      long: 0,
+      data: undefined,
+      meteo: undefined
+    }
+  }
+
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      this.setState({lat: parseFloat(pos.coords.latitude.toFixed(3)), long: parseFloat(pos.coords.longitude.toFixed(3))});
+      fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${APIKey}&q=${this.state.lat}%2C%20${this.state.long}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({data: data});
+        fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${this.state.data.Key}?apikey=${APIKey}&language=fr-FR&metric=true`)
+        .then(res => res.json())
+        .then(data => this.setState({meteo: data}))
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>{this.state.meteo ? this.state.meteo.DailyForecasts[0].Day.IconPhrase : ''}</h1>
       </div>
     );
   }
